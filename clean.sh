0bd9c88c185a4e8d9018807e91cdcdd4e7e468fa
Aadithyan Raju <93834376+AadithyanRaju@users.noreply.github.com>
2023-10-01 22:58:04 +0400

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo
 echo " ██████╗██╗     ███████╗ █████╗ ███╗   ██╗██╗███╗   ██╗ ██████╗     ███████╗ ██████╗██████╗ ██╗██████╗ ████████╗"
 echo "██╔════╝██║     ██╔════╝██╔══██╗████╗  ██║██║████╗  ██║██╔════╝     ██╔════╝██╔════╝██╔══██╗██║██╔══██╗╚══██╔══╝"
 echo "██║     ██║     █████╗  ███████║██╔██╗ ██║██║██╔██╗ ██║██║  ███╗    ███████╗██║     ██████╔╝██║██████╔╝   ██║   "
@@ -10,7 +11,7 @@ echo "Package Manager"
 echo "Pacman cache: $(du -sh /var/cache/pacman/pkg/ | cut -f1)"
 echo "1. Clean Unused Cache"
 echo "2. Clean All Cache"
-echo "3. Cancel"
+echo "3. Skip"
 echo "4. Exit"
 echo
 read -p "Choose an option: " option
@@ -36,7 +37,7 @@ echo "Orphan packages: $(pacman -Qtdq | wc -l)"
 pacman -Qtdq
 echo
 echo "1. Remove Unused Packages"
-echo "2. Cancel"
+echo "2. Skip"
 echo "3. Exit"
 echo
 read -p "Choose an option: " option
@@ -57,7 +58,7 @@ echo
 echo "Home Cache"
 echo "Home cache: $(du -sh $HOME/.cache/ | cut -f1)"
 echo "1. Clean All Cache"
-echo "2. Cancel"
+echo "2. Skip"
 echo "3. Exit"
 echo
 read -p "Choose an option: " option
